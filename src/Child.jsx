8bb2f39306ee191ca2062fcb02553d3c401ec043
Rakibul Islam <rakib.ca@gmail.com>
2024-03-26 19:04:06 -0600

    document.title Update Demo

--- src/Child.jsx
+++ src/Child.jsx
@@ -13,8 +13,12 @@ export function Child() {
   }, []);
 
   useEffect(() => {
-    console.log(`My name is ${name} and I am ${age} years old`);
-  }, [name, age]);
+    document.title = name;
+  }, [name]);
+
+  useEffect(() => {
+    console.log("Re-Render");
+  });
 
   return (
     <div>
