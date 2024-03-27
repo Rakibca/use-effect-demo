527845f863aa2ae9ca1315d485456f30772c2221
Rakibul Islam <rakib.ca@gmail.com>
2024-03-26 19:50:54 -0600

    Timeout Demo

--- src/Child.jsx
+++ src/Child.jsx
@@ -4,24 +4,32 @@ export function Child() {
   const [age, setAge] = useState(0);
   const [name, setName] = useState("");
 
-  useEffect(() => {
-    console.log("Re-Render");
-  });
+  // useEffect(() => {
+  //   console.log("Re-Render");
+  // });
 
-  useEffect(() => {
-    console.log("Hi");
+  // useEffect(() => {
+  //   console.log("Hi");
 
-    return () => {
-      console.log("Bye");
-    };
-  }, []);
+  //   return () => {
+  //     console.log("Bye");
+  //   };
+  // }, []);
 
-  useEffect(() => {
-    console.log(`My name is ${name} and I am ${age} years old`);
-  }, [name, age]);
+  // useEffect(() => {
+  //   console.log(`My name is ${name} and I am ${age} years old`);
+  // }, [name, age]);
 
   useEffect(() => {
     document.title = name;
+
+    const timeout = setTimeout(() => {
+      console.log(`My name is ${name}`);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
   }, [name]);
 
   return (
